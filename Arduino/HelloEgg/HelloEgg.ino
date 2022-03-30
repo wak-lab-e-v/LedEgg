37661d5304b4b267d7ae65cb5704f2de2b26828f
Martin <mhilde@gmx.de>
2022-03-30 22:23:58 +0200

    Blinken etwas langsamer

--- Arduino/HelloEgg/HelloEgg.ino
+++ Arduino/HelloEgg/HelloEgg.ino
@@ -14,25 +14,29 @@ void setup() {
   pinMode(4, INPUT_PULLUP);
   myLedEgg.begin(3);
   myLedEgg.WriteData( 255,  255,  255);
+  for (uint8_t i = 0;i<NUM_LED; i++)
+    myLedEgg.LEDarray[i] = 40;
 }
 
 void loop() {
+  
    static uint16_t delays; 
    // put your main code here, to run repeatedly:
    if (!delays --)
    {
      brightness = brightness +1;
-     delays = 5;
+     delays = 15;
      
      //myLedEgg.LEDarray[0] = brightness;
      for (uint8_t i = 0;i<NUM_LED; i++)
      {
-       myLedEgg.LEDarray[i] = brightness = brightness + 5;
+       myLedEgg.LEDarray[i] = (brightness = brightness + 5)/3;
        if (brightness > 128)
          brightness = brightness - 128;
      }
      //myLedEgg.LEDarray[7] = 255;
    }
+   
    myLedEgg.do_LESs();
    
 }
